8370e7454f6d4deac0dbbdeef5fa372c99a6880b
michelelongo97 <m.longo7x@gmail.com>
2025-01-28 19:02:18 +0100

    feat: add main and first section of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,29 @@
 export default function Header() {
   return (
     <footer>
-      <section className="first-banner">a</section>
-      <section className="second-banner">
-        <img src="../img/footer-bg.jpg" alt="bkg" />
+      <section className="first-banner">
+        <div>
+          <img src="../img/buy-comics-digital-comics.png" />
+          <p>DIGITAL COMICS</p>
+        </div>
+        <div>
+          <img src="../img/buy-comics-merchandise.png" />
+          <p>DC MERCHANDISE</p>
+        </div>
+        <div>
+          <img src="../img/buy-comics-subscriptions.png" />
+          <p>SUBSCRIPTION</p>
+        </div>
+        <div>
+          <img src="../img/buy-comics-shop-locator.png" />
+          <p>COMIC SHOP LOCATOR</p>
+        </div>
+        <div>
+          <img src="../img/buy-comics-digital-comics.png" />
+          <p>DC POWER VISA</p>
+        </div>
       </section>
+      <section className="second-banner"></section>
       <section className="third-banner"></section>
     </footer>
   );
