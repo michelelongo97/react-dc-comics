f38e3d115688c552bc8dc73e4ddd69f963187ae8
michelelongo97 <m.longo7x@gmail.com>
2025-01-28 18:39:31 +0100

    feat: add header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <footer>
+      <section className="first-banner">a</section>
+      <section className="second-banner">
+        <img src="../img/footer-bg.jpg" alt="bkg" />
+      </section>
+      <section className="third-banner"></section>
+    </footer>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+export default function Header() {
+  return (
+    <header className="container">
+      <img src="/img/dc-logo.png" alt="logo" />
+      <nav>
+        <ul>
+          <li>
+            <a href="#">CHARACTER</a>
+          </li>
+          <li>
+            <a href="#">COMICS</a>
+          </li>
+          <li>
+            <a href="#">MOVIES</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">GAMES</a>
+          </li>
+          <li>
+            <a href="#">COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a href="#">FANS</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
