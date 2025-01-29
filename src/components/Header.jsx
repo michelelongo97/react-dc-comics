70d85bb64dc1617e29049035140da265ef8cefb9
michelelongo97 <m.longo7x@gmail.com>
2025-01-29 17:02:54 +0100

    feat: add header's array

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,79 @@
+const links = [
+  {
+    id: 1,
+    text: "CHARACTER",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 2,
+    text: "COMICS",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 3,
+    text: "MOVIES",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 4,
+    text: "TV",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 5,
+    text: "GAMES",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 6,
+    text: "COLLECTIBLES",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 7,
+    text: "VIDEOS",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 8,
+    text: "FANS",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 9,
+    text: "NEWS",
+    url: "#",
+    current: false,
+  },
+  {
+    id: 10,
+    text: "SHOP",
+    url: "#",
+    current: false,
+  },
+];
+
 export default function Header() {
   return (
     <header className="container">
       <img src="/img/dc-logo.png" alt="logo" />
       <nav>
         <ul>
-          <li>
-            <a href="#">CHARACTER</a>
-          </li>
-          <li>
-            <a href="#">COMICS</a>
-          </li>
-          <li>
-            <a href="#">MOVIES</a>
-          </li>
-          <li>
-            <a href="#">TV</a>
-          </li>
-          <li>
-            <a href="#">GAMES</a>
-          </li>
-          <li>
-            <a href="#">COLLECTIBLES</a>
-          </li>
-          <li>
-            <a href="#">VIDEOS</a>
-          </li>
-          <li>
-            <a href="#">FANS</a>
-          </li>
-          <li>
-            <a href="#">NEWS</a>
-          </li>
-          <li>
-            <a href="#">SHOP</a>
-          </li>
+          {links.map((link) => {
+            return (
+              <li key={link.id}>
+                <a href={link.url}>{link.text}</a>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
