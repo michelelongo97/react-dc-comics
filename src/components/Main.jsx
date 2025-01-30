65fac02537e9a292bef935c03065e5c93746d22d
michelelongo97 <m.longo7x@gmail.com>
2025-01-30 20:15:40 +0100

    feat: add main's btns

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,14 @@ export default function Main() {
   return (
     <main>
       <section className="hero"></section>
-      <section className="container">--Content goes here--</section>
+      <section className="container">
+        <div className="current-series">
+          <p>CURRENT SERIES</p>
+        </div>
+        <div className="main-btn-cnt">
+          <button className="main-btn">LOAD MORE</button>
+        </div>
+      </section>
     </main>
   );
 }
